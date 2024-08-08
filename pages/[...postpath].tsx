52414fe8940a46a4efc899df96b8c35f3ce02ef6
Usama Mahmood <139591974+MrUsama07@users.noreply.github.com>
2024-08-09 02:16:30 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https:https://cloudvila.buzz/` + encodeURI(path as string)
+					`https://cloudvila.buzz/` + encodeURI(path as string)
 				}`,
 			},
 		};
